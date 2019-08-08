563b4db79e68eea46b520c7c53fd2024e2eb8646
donghuaibin <844199574@qq.com>
2019-08-08 21:11:54 +0800

    分销页面加入

---
+++ Application/Mobile/Controller/DistributController.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 分销控制器
+ * User: donghuaibin
+ * Date: 2019-08-08
+ * Time: 21:08
+ */
+namespace Mobile\Controller;
+
+class DistributController extends MobileBaseController
+{
+    public function index()
+    {
+        $this->display();
+    }
+
+    public function order_list()
+    {
+        $this->display();
+    }
+
+    public function group()
+    {
+        $this->display();
+    }
+
+    public function rank()
+    {
+        $this->display();
+    }
+}
