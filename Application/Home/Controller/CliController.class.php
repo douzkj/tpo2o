8a8af26ec72b46f9734324f00cc289fe94d517b6
donghuaibin <844199574@qq.com>
2019-08-17 20:27:30 +0800

    拼团订单退回

--- Application/Home/Controller/CliController.class.php
+++ Application/Home/Controller/CliController.class.php
@@ -8,6 +8,7 @@
 namespace Home\Controller;
 
 use Think\Controller;
+use Think\Log;
 
 class CliController extends Controller
 {
@@ -27,17 +28,50 @@ class CliController extends Controller
      */
     public function groupOrderRunner()
     {
-        $group_orders = M('group_order')->where('group_status = 0 and close_at < ' .time() )->select();
+        $now = time();
+        $group_orders = M('group_order')->where('group_status = 0 and close_at <= ' .$now )->select();
         foreach (array_chunk($group_orders, 50) as $group_chunk) {
             foreach ($group_chunk as $group_order) {
-                M('group_order')->where(['id' => $group_order['id']])->save([
-                    'group_status' => 2
-                ]);
-                M('order')->where(['group_order_id' => $group_order['id']])->save([
+                echo "拼团订单【{$group_order['group_order_sn']}】退款中";
+                $this->closeGroupOrder($group_order, $now);
+                echo "拼团订单【{$group_order['group_order_sn']}】退款成功";
+            }
+        }
+    }
+
+    protected function closeGroupOrder($group_order, $time)
+    {
+        M()->startTrans();
+        try {
+            $res1 = M('group_order')->where([
+                'id' => $group_order['id'],
+                'group_status' => 0,
+                'close_at' => ['lte', $time]
+            ])->save([
+                'group_status' => 2
+            ]);
+            $orders = M('order')->lock(true)->where([
+                'pay_status' => 1,
+                'group_order_id' => $group_order['id'],
+                'group_status' => 0
+            ])->select();
+            foreach ($orders as $order) {
+                M('order')->where(['order_id' => $order['order_id']])->save([
                     'group_status' => 2,
                     'order_status' => 3
                 ]);
+                accountLog($order['user_id'], $order['total_amount'], 0, "拼团订单【{$order['order_sn']}】退回金额【{$order['total_amount']}】");
+                echo "订单【{$order['order_sn']}】退回成功";
             }
+            if ($res1) {
+                M()->commit();
+            } else {
+                M()->rollback();
+                echo "拼团订单【{$group_order['group_order_sn']}】无退回";
+            }
+        } catch (\Exception $exception) {
+            M()->rollback();
+            echo "拼团订单【{$group_order['group_order_sn']}】拼团自动结束更新错误:" . $exception->getMessage();
         }
     }
 }
